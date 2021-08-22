517ee4f472143f67a9882190ad69e6d831bc5218
Yussef Rafat <yussefrafat5908@gmail.com>
2021-08-22 17:59:26 -0400

    models all set

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define category name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
